c457a796e98e8862bc6a8271fac9879b3c2f9927
Akshansh Shrivastava <Akshansh773@gmail.com>
2022-06-16 17:15:47 +0530

    build visible

---
+++ client/build/precache-manifest.9cdcb4a9b69930cc7bded90f1d9f8580.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867bb8a2ab04f42fa4e01494a2726327",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b19f78eb9c269dd3453a",
+    "url": "/static/css/2.a40a7a09.chunk.css"
+  },
+  {
+    "revision": "386b4a2bacdae43cda19",
+    "url": "/static/css/main.88a153a4.chunk.css"
+  },
+  {
+    "revision": "b19f78eb9c269dd3453a",
+    "url": "/static/js/2.f9b905a8.chunk.js"
+  },
+  {
+    "revision": "020efe124ab0f60b5e9b1d7806449d07",
+    "url": "/static/js/2.f9b905a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "386b4a2bacdae43cda19",
+    "url": "/static/js/main.e2373cc9.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.e2373cc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "4b9dc077a5e83bc17478662d0144dc92",
+    "url": "/static/media/memories-Logo.4b9dc077.png"
+  },
+  {
+    "revision": "18ebdb6d48b70ab2e72ac3bf9154d8be",
+    "url": "/static/media/memories-Text.18ebdb6d.png"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
