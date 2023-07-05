3fdc2067dad842b45c9975fa153e600e4db4499a
Akshansh Shrivastava <akshansh773@gmail.com>
2023-07-05 14:25:37 +0530

    Update API Endpoint

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getAuth } from "firebase/auth";
 const API = axios.create({
-  baseURL: "https://memories-backend-wqge.onrender.com/posts/",
+  baseURL: "https://memories-backend-wqge.onrender.com/",
   // baseURL: 'http://localhost:5000/',
 });
 
